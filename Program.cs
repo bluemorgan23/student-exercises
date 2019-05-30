93f2c3df06b11778fadb04e6a5f2f8f495e35510
Chris Morgan <bluemorgan2323@gmail.com>
2019-05-30 13:20:20 -0500

    Created 3 cohorts

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace student_exercises
         static void Main(string[] args)
         {
 
-            // Create 4 or more Exercises (arg1 = exercise name, arg2 = langauge)
+            /*
+                Create 4 or more Exercises (arg1 = exercise name, arg2 = langauge)
+             */
+
             Exercise classExercise = new Exercise("Classes and Type Definitions", "C#");
 
             Exercise dictionaryExercise = new Exercise("Dictionaries", "C#");
@@ -18,6 +21,16 @@ namespace student_exercises
             Exercise webScrapingExercise = new Exercise("Data Scraping", "Python");
 
             Console.WriteLine($"{apiExercise.Name}, language: {apiExercise.Language}");
+
+            /*
+                Create 3 or more cohorts
+            */
+
+            Cohort C31 = new Cohort("Cohort 31");
+            Cohort C33 = new Cohort("Cohort 33");
+            Cohort C26 = new Cohort("Cohort 36");
+
+            Console.WriteLine(C31.Name);
         }
     }
 }
