9e9b99a1636ee120be328e8aff35187dc14d0694
Chris Morgan <bluemorgan2323@gmail.com>
2019-05-30 11:50:00 -0500

    Created Student.cs with type declaration.

---
+++ Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace student_exercises
+{
+    class Student {
+        public Student(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Slack {get; set;}
+        public Cohort Cohort {get; set;}
+        public List<Exercise> ExerciseList {get; set;}
+    }
+}
